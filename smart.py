36a9ae93e3f681b5c686374bf890d472325d0766
dounglobalresources1-arch <dounglobalresources1@gmail.com>
2025-10-04 04:34:08 -0800

    Add files via upload
    uploaded version 1

---
+++ smart.py
@@ -0,0 +1,29 @@
+# bub = {
+#     "fullname": "obasi nzubechukwu godwin",
+#     "favourite":  ["egusi soup", "rice"],
+#     "hobbies": ["reading", "drawing","joker"],
+#     "occupation": "student",
+#     "age": 15,
+#     "height": 5.5,
+#     "weight": 66.6,
+#     "gender": "male"        
+# }
+# print(bub["hobbies"])
+
+
+# def  bub  (x , y):
+#      print(x - y)
+
+# bub(20 , 10)  
+
+
+# print("hello, world!")
+
+
+# str1 = "coding looks"
+# str2 = " awesome and "
+# str3 = "very intresting!"
+# result = str1 + str2 + str3
+# print(result)
+
+
